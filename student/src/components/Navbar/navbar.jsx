96b7eabbfa90b49606debe34f9935aa2f7f69011
ayan663 <ayansalgara2018@gmail.com>
2025-07-20 03:23:31 +0530

    ui changes

--- student/src/components/Navbar/navbar.jsx
+++ student/src/components/Navbar/navbar.jsx
@@ -4,12 +4,12 @@ import logo from "../imgages/logo.jpg";
 function navbar() {
   return (
    <>
-   <Navbar fluid rounded>
-      <Navbar.Brand>
+   <Navbar className=''>
+      <Navbar.Brand className='mb-3'>
         <img src= {logo} className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
         <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Carrer Glide</span>
       </Navbar.Brand>
-      <div className="flex md:order-2">
+      {/*<div className="flex md:order-2">
         {/* <Dropdown
           arrowIcon={false}
           inline
@@ -26,32 +26,26 @@ function navbar() {
           <Dropdown.Item>Earnings</Dropdown.Item>
           <Dropdown.Divider />
           <Dropdown.Item>Sign out</Dropdown.Item>
-        </Dropdown> */}
+        </Dropdown> 
         <Navbar.Toggle />
-      </div>
-      <Navbar.Collapse>
-        <Navbar.Link href="/" active>
+      </div>*/}
+      <Navbar.Collapse className=''>
+        <Navbar.Link href="/" className='text-[16px]' active>
           Home
         </Navbar.Link>
-        <Navbar.Link href="/Jobseeker">JobseekerDashboard</Navbar.Link>
-        <Navbar.Link href="/Resumebuilder">Resume builder </Navbar.Link>
-        <Navbar.Link href="/Dashboard">POST JOB </Navbar.Link>
-        <Navbar.Link href="/Signin">Sign in</Navbar.Link>
-        <Navbar.Link href="/joblist">Job Listing</Navbar.Link>
-        <Navbar.Link href="/appli">Applicants</Navbar.Link>
-        <Navbar.Link href="/parser">Parser</Navbar.Link>
-        <Navbar.Link href="/JobInsightsDashboard">JobInsightsDashboard</Navbar.Link>
-        {/* <Navbar.Link href="/Courses">Courses</Navbar.Link> */}
-        <Navbar.Link href="/Meet">Meet</Navbar.Link>
-        <Navbar.Link href="/Resuemeanalyser">ResumeAnalyser</Navbar.Link>
-        <Navbar.Link href="/Udemy">RoadMap</Navbar.Link>
-        <Navbar.Link href="/Newsletter">Newsletter</Navbar.Link>
-        
-
-
-
-
-
+        <Navbar.Link className='text-[16px] whitespace-nowrap' href="/Jobseeker">Jobseeker Dashboard</Navbar.Link>
+        <Navbar.Link className='text-[16px] whitespace-nowrap' href="/Resumebuilder">Resume builder </Navbar.Link>
+        <Navbar.Link className='text-[16px] whitespace-nowrap' href="/Dashboard">POST JOB </Navbar.Link>
+        <Navbar.Link className='text-[16px] whitespace-nowrap' href="/Signin">Sign in</Navbar.Link>
+        <Navbar.Link className='text-[16px] whitespace-nowrap' href="/joblist">Job Listing</Navbar.Link>
+        <Navbar.Link className='text-[16px] whitespace-nowrap' href="/appli">Applicants</Navbar.Link>
+        <Navbar.Link className='text-[16px] whitespace-nowrap' href="/parser">Parser</Navbar.Link>
+        <Navbar.Link className='text-[16px] whitespace-nowrap' href="/JobInsightsDashboard">JobInsights Dashboard</Navbar.Link>
+        {/* <Navbar. className='text-[16px] whitespace-nowrap'Link href="/Courses">Courses</Navbar.Link> */}
+        <Navbar.Link className='text-[16px] whitespace-nowrap' href="/Meet">Meet</Navbar.Link>
+        <Navbar.Link className='text-[16px] whitespace-nowrap' href="/Resuemeanalyser">Resume Analyser</Navbar.Link>
+        <Navbar.Link className='text-[16px] whitespace-nowrap' href="/Udemy">Roadmap</Navbar.Link>
+        <Navbar.Link className='text-[16px] whitespace-nowrap' href="/Newsletter">Newsletter</Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
    </>
